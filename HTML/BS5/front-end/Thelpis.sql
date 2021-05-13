612d7790a09049e84dfec860f1c3956962ceaa4b
Nana <NanaBoakye1738@gmail.com>
2021-05-13 10:35:10 +0000

    Revert "Added a new assets folder as well as added blog feature with CRUD"
    This reverts commit ec4fc0b2137c22fa56091cb7c104ce3ae7ab1a8c.

--- HTML/BS5/front-end/Thelpis.sql
+++ HTML/BS5/front-end/Thelpis.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS THELPIS;
 CREATE DATABASE THELPIS;
 USE THELPIS;
+
 CREATE TABLE user_login(
     UserID INT AUTO_INCREMENT,
     Email VARCHAR(70) UNIQUE,
@@ -8,16 +9,10 @@ CREATE TABLE user_login(
     Pass VARCHAR(150),
     primary key(UserID)
 );
+
 CREATE TABLE data(
     id INT AUTO_INCREMENT,
     title VARCHAR (255),
     content LONGBLOB,
     primary key(id)
-);
-CREATE TABLE admin(
-    AdminID INT AUTO_INCREMENT,
-    Admin_Email VARCHAR(70) UNIQUE,
-    AdminName VARCHAR(150),
-    Pass VACRHAR(150),
-    primary key(AdminID) 
 );
